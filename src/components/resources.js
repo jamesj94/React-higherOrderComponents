c5b4fdc5a826255d647ab3239cf8842657e09252
jamesj94 <jhrayj@gmail.com>
2018-01-23 22:24:52 -0800

    Added resources tab, and Router, and Routes

---
+++ src/components/resources.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default () => {
+  return(
+    <div>
+      Super awesome hidden secrets
+      <ul>
+        <li>Americans</li>
+        <li>Simply</li>
+        <li>Love</li>
+        <li>Their</li>
+        <li>Ovaltine</li>
+      </ul>
+    </div>
+  );
+};
